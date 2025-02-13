0af8e6c814914fc10767a1d65094dd8b0a686e5f
Jay Prakash <jayprakash1200@gmail.com>
2025-02-13 16:16:09 +0530

    .env setup

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectToDb = async () => {
   try {
     const res = await mongoose.connect(
-      "mongodb+srv://jayprakash1200:Jay%4017423@cluster0.w6svg.mongodb.net/fileUploader"
+      process.env.MONGO_DB_URL
     );
     if (res) {
       console.log(" Database connect successfully");
